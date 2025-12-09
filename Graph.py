d0f9e2f3a78d0e6713943c7486b9c7052ba81e34
fredr <fredrik.amcoff@hotmail.com>
2025-12-10 00:00:11 +0100

    experimental

--- Graph.py
+++ Graph.py
@@ -1522,16 +1522,64 @@ class Graph(QtWidgets.QWidget):
 
 
 app = QtWidgets.QApplication(sys.argv)
-viewer = Graph(xmin=-1, xmax=1, ymin=-1, ymax=1)
-viewer.resize(900, 900)
+viewer = GraphWindow(rows=1, cols=2)
+graph1 = Graph(ymin=0, ymax=10)
+graph2 = Graph(xmin=0, xmax=10, ymin=-1, ymax=1, y_axis=False)
+viewer.add_graph(graph1)
+viewer.add_graph(graph2)
+#a = viewer.add_parameter("a", 0, 1, 1, 200)
+#b = viewer.add_parameter("b", 0, 10, 5, 100, 'real')
+c = viewer.add_parameter("c", 0, 50, 5, 100, 'real')
+#p = viewer.add_parameter("p", 0, 1, 0, 100, numeric_domain='real')
+#n = viewer.add_parameter("n", 0, 1000, 2, 1000, numeric_domain='integer')
+b = viewer.add_parameter("b", 0, 10, 1, 100, numeric_domain='real')
+mu = viewer.add_parameter("mu", -10, 10, 0, 100, numeric_domain='real')
+sigma2 = viewer.add_parameter("sigma2", 0, 10, 1, 100, numeric_domain='real')
 
-a = viewer.add_parameter("a", 0, 1, 1, 50)
-b = viewer.add_parameter("b", 0, 100, 15, 100)
-e = viewer.add_parameter("e", 0, 10, 5, 100)
-f = viewer.add_parameter("f", 0, 10, 5, 100)
-R = 6
-r = 1
-d = 1
+#graph2.add_point(a, b)
+#graph1.add_grid(x_range=(-10, 10), y_range=(-10, 10), num_lines=11, x_func=lambda x,y,a: (1-a)*x + a*0, y_func=lambda x,y,a: (1-a)*y + a*((x+y)/2), color="b")
+
+interval = sp.FiniteSet(*range(500))
+interval3 = sp.Interval(0, 6) - sp.FiniteSet(1)
+interval2 = sp.Interval(1, c) & sp.Integers
+xs = np.linspace(0, 15, 100)
+
+binomial_interval = sp.Interval(0, 100) & sp.Integers
+exponential_interval = sp.Interval(0, oo, left_open=True)
+exponential_set = graph1.add_set(exponential_interval)
+normal_interval = sp.Interval(-oo, oo)
+normal_set = graph1.add_set(normal_interval)
+
+interval2_set = graph1.add_set(interval2)
+interval_set = graph1.add_set(interval)
+interval3_set = graph1.add_set(interval3)
+
+binomial_set = graph1.add_set(binomial_interval)
+
+#graph1.add_function(lambda x, a, b: (a*x)**b, num_points=1000, domain=interval2_set)
+graph1.add_function(lambda x, mu, sigma2: (1/(sp.sqrt(2*sp.pi*sigma2)))*exp(-(1/2)*((x-mu)**2))/(2*sigma2), num_points=1000, domain=normal_set)
+
+
+
+#viewer.resize(900, 900)
+
+#x_lower = -100
+#y_lower = -100
+#x_upper = 100
+#y_upper = 100
+
+#a = viewer.add_parameter("a", 0, 1, 0, 200)
+#b = viewer.add_parameter("b", 0, 100, 0, 100)
+#e = viewer.add_parameter("e", 0, 10, 5, 100)
+#f = viewer.add_parameter("f", 0, 10, 5, 100)
+
+#U = lambda x, y: x / (x + y)
+#V = lambda x, y: x + y
+#X = lambda x, y: (y*x)/(1-x)
+#Y = lambda x, y: x - y
+
+#viewer.add_grid(y_func=lambda x, y, a, b: (a)*x + (1-a)*(U(x, y)) + b, x_func=lambda x, y, a: (a)*y + (1-a)*(V(x,y)), params={"a": a}, x_range=(x_lower, x_upper), y_range=(y_lower, y_upper), color="b", alpha=0.1)
+
 #viewer.add_grid(transform_func=lambda x, y, c, d, e, f: (e+c*sp.sin(x), f+d*c*sp.sin(y)), params={"c": a, "d": b})
 viewer.add_function(lambda x, b: (b-r)*cos(x) + d*cos(((b-r)/r)*x), x_func=lambda x, b: (b-r)*sin(x) + d*sin(((b-r)/r)*x), num_points=1000, t_range=(0, 2*np.pi), width=1)
 f = viewer.add_function(lambda x, a: sin(a*x), t_range=(-10, 10), num_points=100)
